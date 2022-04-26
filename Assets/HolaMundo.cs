ed69041109e8b9eb768c8b802dfe61f674d79dad
UrielJolodovsky <46960341@est.ort.edu.ar>
2022-04-26 09:00:36 -0300

    Initial commit

---
+++ Assets/HolaMundo.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HolaMundo : MonoBehaviour
+{
+    public int num1;
+    public int num2;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        if (num1 > num2)
+        {
+            int a = num1;
+            while (num1 >= a && a >= num2)
+            {
+                Debug.Log(a);
+                a--;
+            }
+        }
+        else
+        {
+            Debug.Log("Vuelva a escribir los números");
+        }
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+   public void Holamundo()
+    {
+        int counter = 1;
+        while (counter <= 100)
+        {
+            Debug.Log(counter);
+            counter++;
+        }
+
+    }
+}
